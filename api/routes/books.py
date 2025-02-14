1d0170be8e5bcf1080c07a035087a2e430f90c90
Humaidu <humaiduali@gmail.com>
2025-02-14 06:28:33 +0000

    updated get books by id endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -47,18 +47,14 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
-async def get_book(book_id: int) -> Book:
-    book = db.get_book(book_id)
-    if book:
-        return JSONResponse(
-                status_code=status.HTTP_200_OK,
-                content=book.model_dump()
-                )
-    else:
-        return JSONResponse(
-                status_code=status.HTTP_404_NOT_FOUND,
-                content={"details": "NOT FOUND"}
-                )
+async def get_book_by_id(book_id: int):
+  book = db.books.get(book_id)
+  if not book:
+    return JSONResponse(
+      status_code=status.HTTP_404_NOT_FOUND,
+      content={"detail": "Book not found"},
+    )
+  return book
                 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
